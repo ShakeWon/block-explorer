298a39918119d7f7e9c692661b8f9ee7308ccb75
linchen <linchen@zhongan.io>
2018-05-31 18:57:49 +0800

    search

--- controller/SearchController.go
+++ controller/SearchController.go
@@ -5,6 +5,7 @@ import (
     "github.com/shakewon/block-explorer/service"
     "github.com/shakewon/block-explorer/model/vo/response"
     "github.com/kataras/iris/core/errors"
+    "strconv"
 )
 
 var (
@@ -13,8 +14,8 @@ var (
 )
 
 type SearchController struct {
-    Ctx iris.Context
-    BlockSerivce service.BlockService
+    Ctx                iris.Context
+    BlockSerivce       service.BlockService
     TransactionService service.TransactionService
 }
 
@@ -23,5 +24,23 @@ func (s *SearchController) GetSearch() {
     if len(keywords) == 0 {
         s.Ctx.JSON(response.BaseResponse{Success: false, Error: errors.New("keywords can not be null")})
         return
+    }
+
+    if h, error := strconv.Atoi(keywords); error != nil {
+        //hash search
+        if data, error := s.BlockSerivce.Page(1, 1, string(h), ""); error != nil {
+            s.Ctx.JSON(response.BaseResponse{Success: false, Error: error})
+            return
+        } else {
+            s.Ctx.JSON(response.BaseResponse{Success: false, Data: response.SearchResponse{Type: BLOCK, Data: data}})
+        }
+    } else {
+        if data, error := s.TransactionService.Search(keywords);error!=nil{
+            s.Ctx.JSON(response.BaseResponse{Success: false, Error: error})
+            return
+        }else {
+            s.Ctx.JSON(response.BaseResponse{Success: false, Data: response.SearchResponse{Type: TRANSACTION, Data: data}})
+        }
+
     }
 }
