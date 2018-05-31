3bf683b07f85fae080fea108ff4b267d14ced78c
linchen <linchen@zhongan.io>
2018-05-31 20:27:30 +0800

    log

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func main() {
 
     app.Logger().Handle(func(l *golog.Log) bool {
         prefix := golog.GetTextForLevel(l.Level, true)
-        pc, fn, line, _ := runtime.Caller(7)
+        pc, fn, line, _ := runtime.Caller(6)
         message := fmt.Sprintf("%s line %d (%s) (%s) %s: %s",
             prefix, line, runtime.FuncForPC(pc).Name(), fn, l.FormatTime(), l.Message)
 
